2ce5ddb58de2377ada3d8c90476f779bd6eca581
COR Students <corstudents@Matts-MBP-77.localdomain>
2018-11-08 12:49:15 -0500

    tic tac toe game

---
+++ tic-tac-toe.js
@@ -0,0 +1,70 @@
+//Game starts with X
+function startGame() {
+    for(i = 1; i <= 9; i++) {
+        clearBox[i]
+    }
+    document.turn = "X";
+    document.winner = null;
+    setMessage(document.turn + " get's to start.")
+}
+//sets a message
+function setMessage(msg){
+    document.getElementById("message").innerText = msg;
+}
+//X and O are switching, tells you to pick another square 
+function nextMove(square) {
+    if(document.winner != null){
+        setMessage(document.turn + " already won!")
+    } else if(square.innerText == "") {
+    square.innerText = document.turn;
+    switchTurn();
+    } else {
+    setMessage("Pick another square")
+}
+}
+//switchs turn and sets a message on who goes next
+function switchTurn() {
+    if(checkForWinner(document.turn)) {
+        setMessage("Congrats " + document.turn + ", you won!")
+        document.winner = document.turn;
+    } else if(document.turn == "X") {
+        document.turn = "O";
+        setMessage("It's " + document.turn + "'s turn.");
+    } else {
+        document.turn = "X";
+        setMessage("It's " + document.turn + "'s turn.");  
+    }
+    return result
+}
+//Check for winner
+function checkForWinner(move) {
+    let result = false;
+    if(checkRow(1, 2, 3, move) ||
+       checkRow(4, 5, 6, move) ||
+       checkRow(7, 8, 9, move) ||
+       checkRow(1, 4, 7, move) ||
+       checkRow(2, 5, 8, move) ||
+       checkRow(3, 6, 9, move) ||
+       checkRow(1, 5, 9, move) ||
+       checkRow(3, 5, 7, move)) {
+          result = true;
+    }
+    return result
+}
+
+//check the rows when clicked
+function checkRow(a, b, c, move) {
+    let result = false;
+    if(getBox(a) == move && getBox(b) == move && getBox(c) == move){
+        result = true;
+    }
+    return result;
+}
+//Return the value of the element, if it is an X or O
+function getBox(number) {
+    return document.getElementById("s" + number).innerText;
+}
+
+function clearBox(number) {
+    document.getElementById("s" + number).innerText = "";
+}
